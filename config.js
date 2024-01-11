97c0b986eb65dc03cca35e3134106882d513962c
Chamudithalaksha <156283080+Chamudithalaksha@users.noreply.github.com>
2024-01-11 12:11:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['917605902011', '𝙲𝙷𝙰𝙼𝚄', true],
+  ['6281255369012', '𝙲𝙷𝙰𝙼𝚄', true], 
   [''] 
 ] //Number of owners
 
@@ -50,9 +50,9 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'ᴛʜᴇ 𝙷𝙸𝙹𝙰𝙺 𝙲𝙷𝙰𝙼𝚄𝚆𝙰'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
+global.packname = '𝙷𝙸𝙹𝙰𝙺 𝙲𝙷𝙰𝙼𝚄𝚆𝙰┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
